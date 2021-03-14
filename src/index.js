3f763122a786aab5336ca4e8d01f9920f56ab515
Ivan Kuper <wano9715@gmail.com>
2021-03-14 13:05:51 +0300

    feat: add min

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (array !== undefined) {
+    return array.reduce((pivot, item) => (pivot < item ? pivot : item), 0);
+  }
   return 0;
-}
+};
 
-exports.max = function max (array) {
+exports.max = function max(array) {
+  if (array !== undefined) {
+    return array.reduce((pivot, item) => (pivot > item ? pivot : item), 0);
+  }
   return 0;
-}
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  return array.reduce((prev, item) => prev + item, 0) / array.length;
+};
